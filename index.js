d4fe73771fbafae2aed37cd246eaf18ad1d8b04e
varsha-kadaru <107875699+varsha-kadaru@users.noreply.github.com>
2023-07-05 19:14:10 +0530

    fixed

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 const port = 8081;
 
-app.use(express.json);
+app.use(express.json());
 
 app.get("/" , (req,res) =>{
-    res.status(200).json({
+    res.status(200).send({
         message : "Running succesfully"
     });
 });
